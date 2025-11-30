bca8b7d96699207c351552ce0911c010b6ce111b
Martin <149235488+joma65@users.noreply.github.com>
2025-11-29 22:09:43 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,50 @@
+// Updated version to force cache refresh for new UI
+const CACHE_NAME = 'weather-compare';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  // Removed local icons from cache list since we use external URL
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/lucide@latest'
+];
+
+// Install Event: Cache assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[Service Worker] Caching all: app shell and content');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Fetch Event: Serve from cache, fallback to network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Return cache hit if found
+      if (response) {
+        return response;
+      }
+      // Otherwise fetch from network
+      return fetch(event.request);
+    })
+  );
+});
+
+// Activate Event: Cleanup old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          // Delete old cache versions (v1, v2, v3, etc)
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
